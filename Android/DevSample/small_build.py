ab2c53d2bc27ba6462440094d11ee5d0bdb9ed52
peacepassion <daqi1990@gmail.com>
2016-04-19 10:47:37 +0800

    add python script for one command build

---
+++ Android/DevSample/small_build.py
@@ -0,0 +1,25 @@
+# /usr/bin/python
+import subprocess
+
+from subprocess import Popen
+
+
+def run(*args):
+    p = Popen(*args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while True:
+        line = p.stdout.readline()
+        print line.replace('\n', '')
+        if not line:
+            break
+    return p.wait()
+
+
+if __name__ == '__main__':
+    tasks = list()
+    tasks.append(['gradle', 'clean', 'cleanLib', 'cleanBundle'])
+    tasks.append(['gradle', 'buildLib'])
+    tasks.append(['gradle', 'buildBundle'])
+    tasks.append(['gradle', ':app:aR'])
+    for task in tasks:
+        if run(task):
+            break
